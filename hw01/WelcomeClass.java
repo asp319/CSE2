ea44c77327b83be36198978a14db7111257c5a6d
asp319 <asp319@lehigh.edu>
2015-08-29 14:48:57 +0000

    I completed welcome class homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Ashini Patel
+//hw01
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the design in the parenthesis 
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-A--S--P--3--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My favorite color is forest green, and my favorite type of food is diner food. ");
+    
+    
+    //test/test
+    }
+    
+}
+
+
+
+
+  
